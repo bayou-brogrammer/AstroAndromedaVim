f7de0e0e25ef85abe6fa7f6fb306dff20ff187cf
Jacob LeCoq <lecoqjacob@gmail.com>
2023-12-17 21:55:43 -0600

    cleanup

--- lua/modules/plugins/completion.lua
+++ lua/modules/plugins/completion.lua
@@ -33,12 +33,12 @@ cmp_plugins["zbirenbaum/copilot.lua"] = {
   opts = {
     suggestion = {
       keymap = {
-        accept = "<C-l>",
-        accept_word = false,
-        accept_line = false,
         next = "<C-.>",
         prev = "<C-,>",
+        accept = "<C-l>",
         dismiss = "<C/>",
+        accept_word = false,
+        accept_line = false,
       },
     },
   },
